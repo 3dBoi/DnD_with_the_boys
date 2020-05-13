d1273c67038e5715d09ae541da709305cf169a5a
3dBoi <munir.ali@gmx.de>
2020-05-14 01:03:20 +0200

    Neu Tolle Ordner Struktur und Spagetti Code von mir

---
+++ src/com/company/EquipmentCard.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.*;
+
+public class EquipmentCard extends ItemCard {
+
+    private int attack;
+    private double defence;
+
+    public EquipmentCard(int attack, double defence) {
+        this.attack = attack;
+        this.defence = defence;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public double getDefence() {
+        return defence;
+    }
+
+    public void setDefence(double defence) {
+        this.defence = defence;
+        EquipmentList.add(Dagger);
+    }
+
+
+
+    EquipmentCard Dagger = new EquipmentCard(5, 1);
+    EquipmentCard Club = new EquipmentCard(8, 1);
+    EquipmentCard Mace = new EquipmentCard(11, 1);
+    EquipmentCard Sword = new EquipmentCard(14, 1);
+    EquipmentCard Spear = new EquipmentCard(17,1);
+    EquipmentCard Axe = new EquipmentCard(20, 1);
+    EquipmentCard Warhammer = new EquipmentCard(24, 1);
+
+    ArrayList<EquipmentCard> EquipmentList = new ArrayList<EquipmentCard>();
+
+
+    Player player = new Player(100,100,2,10,10,5);
+
+
+    public void equip(EquipmentCard Weapon){
+
+
+
+
+
+
+
+       com.company.Combat.player.setMaxattack(com.company.Combat.player.getMaxattack() + Weapon.getAttack());
+
+    }
+
+    public void unequip(EquipmentCard Weapon){
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File data = new File("Datenbank.txt");
+        Scanner S= new Scanner(data);
+
+
+    }
+}
