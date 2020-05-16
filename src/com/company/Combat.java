76d8a5786453b3973492d448ce3318fae2557b21
Fitzi99 <64744600+Fitzi99@users.noreply.github.com>
2020-05-16 12:40:18 +0200

    Update Combat.java

--- src/com/company/Combat.java
+++ src/com/company/Combat.java
@@ -1,38 +1,36 @@
 package com.company;
 import java.util.*;
 
-public class Combat {
+public class Fight {
+	static boolean playerattack;
 
 	
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> moveset = new ArrayList<Integer>(Arrays.asList(1,2,1));
 		Player player = new Player(100,100,2,10,10,5);
-		EnemyCard enemy = new EnemyCard(100,100,2,10,10,5,moveset);
-		comabtSequence(player, enemy, moveset);
+		EnemyCard enemy = new EnemyCard(100,100,2,10,10,5,new ArrayList<Integer>(Arrays.asList(1,2,1)));
+		comabtSequence(player, enemy);
 	}
 	
-	public static void comabtSequence(Player player, EnemyCard enemy, ArrayList<Integer> moveset) {
+	public static void comabtSequence(Player player, EnemyCard enemy) {
 		
 		Scanner s = new Scanner(System.in);
 		int counter = 0;
-		boolean playerattack = false;
+		playerattack = false;
 		
 		while(player.getHealth()>0 && enemy.getHealth()>0) {
 			
 			System.out.println("1. attack \n2. defend \n3. Item");
 			int playermove = s.nextInt();
 			
-			if(doPlayerMove(player, enemy, playermove)>0) {
-				playerattack = true;
-			}
-			
-			if(counter < moveset.size()) {
-				int enemymove = moveset.get(counter);
+			doPlayerMove(player, enemy, playermove);
+						
+			if(counter < enemy.getMoveset().size()) {
+				int enemymove = enemy.getMoveset().get(counter);
 				doEnemyMove(player, enemy, enemymove);
 			}else {
 				counter=0;
-				int enemymove = moveset.get(counter);
+				int enemymove = enemy.getMoveset().get(counter);
 				doEnemyMove(player, enemy, enemymove);			
 			}
 			
@@ -60,15 +58,14 @@ public class Combat {
 		}
 	}
 	//Aktion des Spielers 1 ist Angriff 2 ist verteidigen 3 ist Gegenstand
-	public static int doPlayerMove(Player player, EnemyCard enemy, int playermove) {
+	public static void doPlayerMove(Player player, EnemyCard enemy, int playermove) {
 		
 		if(playermove==1) {
-			return 1;
+			playerattack = true;
 		}else if(playermove==2){
 			enemy.setAttack((int) (enemy.getAttack()/player.performDefence()));
-			return 0;
 		}else {
-			return 0;
+			
 		}
 	}
 	
