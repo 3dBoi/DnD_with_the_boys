45881d571789351a5576e475aefcbaa689dc3821
Janisch <janiskarl@web.de>
2020-05-18 18:30:51 +0200

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,7 @@
  */
 package com.company;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,10 +14,6 @@ import javafx.scene.Scene;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 
 
 public class Main extends Application {
@@ -31,7 +24,7 @@ public class Main extends Application {
    
         //Ein neues Fenster mit einer Scene wird erstellt
         Parent root;
-        root = FXMLLoader.load(getClass().getResource("FXMLMainMenue.fxml"));
+        root = FXMLLoader.load(getClass().getResource("FXMLMainMenu.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -40,6 +33,7 @@ public class Main extends Application {
         
     }
     
+    /*
     //Play Main Music
      public static MediaPlayer musicplayer; {
     Media mainTheme = null;
@@ -50,30 +44,16 @@ public class Main extends Application {
         }
     musicplayer = new MediaPlayer(mainTheme);
     musicplayer.setAutoPlay(true);
-    musicplayer.setVolume(0.6);
+    musicplayer.setVolume(0.5);
 }
-
+*/
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //launch(args);
-
-        HashMap<String, EquipmentCard> equipmentCardHashMap = new HashMap<>();
-        try {
-            JSONTokener tokener = new JSONTokener(new FileReader("./src/com/resources/Datenbank.json"));
-            JSONObject object = new JSONObject(tokener);
-            JSONArray items = object.getJSONArray("Items");
-            for (Object o: items) {
-                JSONObject json = (JSONObject) o;       //Schlüssel durch Card id ersetzen
-                equipmentCardHashMap.put((String) json.get("name"), new EquipmentCard((String) json.get("name"), (int) json.get("attack"), (double)(Integer) json.get("defence"), (int) json.get("slot"), (int)json.get("critAdd")));
-             //   equipmentCardHashMap.put((String) json.get("name"), new EquipmentCard((String) json.get("name"), (int) json.get("attack"), (double) json.get("defence"), (int) json.get("slot")));
-            }
-        } catch (FileNotFoundException f) {
-            System.out.println("FileNotFound: " + f.getLocalizedMessage());
-        } catch (JSONException e) {
-            System.out.println("JSONException: " + e.getLocalizedMessage());
-        }
+                FXMLMainMenuController.jukeboxMain.playTypecast();
+        launch(args);
     }
     
 
